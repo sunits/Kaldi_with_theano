88219d8298563d1760a02a2dbc48a02f43dc667d
Sunit <me@sunits.in>
2016-07-28 16:08:11 +0200

    comments

--- nnet1_with_theano/nnet_train.sh
+++ nnet1_with_theano/nnet_train.sh
@@ -1,5 +1,5 @@
 echo `hostname`
-GPU='gpu1'
+GPU='gpu0'
 
 # Trained RBM
 DNN_BASEPATH='/talc/multispeech/calcul/users/ssivasankaran/kaldi_exp/chime3_new/s5/exp/reverb_test/puma_data/reverb/clean_align/layers/3/tri4a_dnn_pretrain_tr05_multi_reverbed/'
@@ -23,7 +23,7 @@ mkdir -p $model_path
 
 
 # Keep a local copy and avoid reading from NFS
-# If you are not using NFS you can disable it
+# If you are not using NFS you can disable it, Make sure the temp paths are set to the original paths
 temp_train_feats='/tmp/tr05_multi_reverbed_feats.hdf5'
 temp_valid_feats='/tmp/dt05_multi_reverbed_feats.hdf5'
 
